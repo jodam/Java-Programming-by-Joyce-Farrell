7d2e47961fe3b69db584d9dd04298e3e90f2716d
jodam <45196841+jodam@users.noreply.github.com>
2023-01-03 00:43:49 -0500

    Add files via upload

---
+++ CarlysMotto2.java
@@ -0,0 +1,6 @@
+import javax.swing.JOptionPane;
+public class CarlysMotto2{
+	public static void main(String[] args){
+		JOptionPane.showMessageDialog(null, "****************************************\nCarly's makes the food that makes it a party.\n****************************************");
+	}
+}
