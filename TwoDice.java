495ea0ccdf6a04f6db89e053eeddec3fb7ba6a39
jodam <45196841+jodam@users.noreply.github.com>
2023-01-06 08:57:23 -0500

    Add files via upload

---
+++ TwoDice.java
@@ -0,0 +1,9 @@
+public class TwoDice{
+	public static void main(String[] args){
+		Die firstThrow = new Die();
+		Die secondThrow = new Die();
+				
+		firstThrow.display();
+		secondThrow.display();
+	}
+}
