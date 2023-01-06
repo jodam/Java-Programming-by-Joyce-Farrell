3156460900a38831890a255658bbf2419c2217d3
jodam <45196841+jodam@users.noreply.github.com>
2023-01-06 08:56:48 -0500

    Add files via upload

---
+++ FiveDice.java
@@ -0,0 +1,15 @@
+public class FiveDice{
+	public static void main(String[] args){
+		Die firstThrow = new Die();
+		Die secondThrow = new Die();
+		Die thirdThrow = new Die();
+		Die fourthThrow = new Die();
+		Die fifthThrow = new Die();
+		
+		firstThrow.display();
+		secondThrow.display();
+		thirdThrow.display();
+		fourthThrow.display();
+		fifthThrow.display();
+	}
+}
